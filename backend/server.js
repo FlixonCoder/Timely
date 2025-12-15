3cb3fb97c336ab14d83234d358661467003190e0
SaqJun <bdhdvzg@gmail.com>
2025-12-16 00:44:22 +0530

    m

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://timily.vercel.app/",
+      "https://timilyy.vercel.app/",
       "http://localhost:5173"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
